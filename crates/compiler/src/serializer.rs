b2cd771b75808d6da8e6cf039881801513eb1d12
xazukx <>
2025-12-02 19:15:01 -0700

    Make serializing values public

--- crates/compiler/src/serializer.rs
+++ crates/compiler/src/serializer.rs
@@ -59,7 +59,7 @@ pub(crate) fn serialize_number(
     Ok(serializer.finish_for_expr())
 }
 
-pub(crate) fn serialize_value(val: &Value, options: &Options, span: Span) -> SassResult<String> {
+pub fn serialize_value(val: &Value, options: &Options, span: Span) -> SassResult<String> {
     let map = CodeMap::new();
     let mut serializer = StyleSerializer::new(options, &map, false, span);
 
